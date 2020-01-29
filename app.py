8ac2e2fdf2c3d8ec73c639f6feeeac20c05f35b4
Kufooloo <robot2boy@icloud.com>
2020-01-29 13:04:28 -0800

    Deleted index added home changed app

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
-
-
-@app.route('/')
-def hello():
-    return "Hello World!"
-
+@app.route('/home')
+def home():
+    return render_template('home.html')
+@app.route('/home', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    processed_text = text.upper()
+    return processed_text
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
